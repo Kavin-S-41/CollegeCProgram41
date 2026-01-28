41c7cb5020ba85c9c648efda71f82b8590eb9672
Kavin S <kavin.finaldraft@gmail.com>
2026-01-28 15:31:47 +0530

    Add files via upload

---
+++ Expt1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b,sum;
+   printf("Enter Two NO : ");
+   scanf("%d %d",&a,&b);
+   sum=a+b;
+   printf("The sum is %d",sum);
+   getchar();
+   getchar();
+   return 0;
+}
